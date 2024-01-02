d4d63770df29d55f7912d53e81dfcd63f5f4be55
rarz <115542704+rniarzz@users.noreply.github.com>
2024-01-02 22:27:16 +0700

    Update ubah.php

--- checklist/ubah.php
+++ checklist/ubah.php
@@ -45,8 +45,8 @@ if (isset($_POST['submit']))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
-<body style="margin-top: 30px; background-color: #0A2647;">
-    <div class="container" style="background-color: #144272; width: 50%; padding: 30px;">
+<body style="margin-top: 30px; background-color: #778899;">
+    <div class="container" style="background-color: #FF69B4; width: 50%; padding: 30px;">
         <h1 style="color: #FFFFFF; text-align: center;">Ubah Data</h1>
         <div class="main">
             <form method="post" action="ubah.php" enctype="multipart/form-data">
@@ -119,7 +119,7 @@ if (isset($_POST['submit']))
                 </div>
                 <div>
                 <input type="hidden" name="id" value="<?php echo $data['id'];?>">
-                <input class="tombol" type="submit" name="submit" value="Simpan"/>
+                 <input class= "btn" style="background-color: #778899; color: #FFFFFF;" type="submit" name="submit" value="Simpan">
                 </div>
             </form>
         </div>
